8612be0d5e73e6944b1a5f86f89e20f0bcdedcc6
hinzundcode <chris-git@labasch.net>
2021-01-20 02:27:49 +0100

    print landscape a5 papers

--- cyberdesk/paperspace/__init__.py
+++ cyberdesk/paperspace/__init__.py
@@ -94,16 +94,13 @@ DIN_A5_HEIGHT_POINTS = 595
 def mm_to_points(x, page_width_mm=DIN_A4_WIDTH_MM, page_width_points=DIN_A4_WIDTH_POINTS):
 	return x * page_width_points/page_width_mm
 
-def draw_rect_portrait(ctx, markers, title="",
-	page_width_mm=DIN_A4_WIDTH_MM, page_height_mm=DIN_A4_HEIGHT_MM,
-	page_width_points=DIN_A4_WIDTH_POINTS):
-	
+def draw_rect_portrait_a4(ctx, markers, title=""):
 	tl = create_monochrome_surface(markers[0], markers[0].shape, 0)
 	tr = create_monochrome_surface(markers[1], markers[1].shape, 1)
 	br = create_monochrome_surface(markers[2], markers[2].shape, 2)
 	bl = create_monochrome_surface(markers[3], markers[3].shape, 3)
 	
-	page_size = (page_width_mm, page_height_mm)
+	page_size = (DIN_A4_WIDTH_MM, DIN_A4_HEIGHT_MM)
 	border = (10, 10)
 	marker_size = (30, 30)
 
@@ -112,33 +109,52 @@ def draw_rect_portrait(ctx, markers, title="",
 	br_pos = (page_size[0] - marker_size[0] - border[0], page_size[1] - marker_size[1] - border[1])
 	bl_pos = (border[0], page_size[1] - marker_size[1] - border[1])
 
-	page = {"page_width_mm": page_width_mm, "page_width_points": page_width_points}
-
-	marker_size_points = (mm_to_points(marker_size[0], **page), mm_to_points(marker_size[1], **page))
+	marker_size_points = (mm_to_points(marker_size[0]), mm_to_points(marker_size[1]))
 	
-	draw_image(ctx, tl, (mm_to_points(tl_pos[0], **page), mm_to_points(tl_pos[1], **page)), marker_size_points)
-	draw_image(ctx, tr, (mm_to_points(tr_pos[0], **page), mm_to_points(tr_pos[1], **page)), marker_size_points)
-	draw_image(ctx, br, (mm_to_points(br_pos[0], **page), mm_to_points(br_pos[1], **page)), marker_size_points)
-	draw_image(ctx, bl, (mm_to_points(bl_pos[0], **page), mm_to_points(bl_pos[1], **page)), marker_size_points)
+	draw_image(ctx, tl, (mm_to_points(tl_pos[0]), mm_to_points(tl_pos[1])), marker_size_points)
+	draw_image(ctx, tr, (mm_to_points(tr_pos[0]), mm_to_points(tr_pos[1])), marker_size_points)
+	draw_image(ctx, br, (mm_to_points(br_pos[0]), mm_to_points(br_pos[1])), marker_size_points)
+	draw_image(ctx, bl, (mm_to_points(bl_pos[0]), mm_to_points(bl_pos[1])), marker_size_points)
 	
 	if title != "":
 		ctx.set_font_size(15)
 		extents = ctx.text_extents(title)
 		ctx.move_to(
-			mm_to_points(page_size[0]/2, **page) - extents.width/2,
-			mm_to_points(page_size[1] - border[1]/2, **page)
+			mm_to_points(page_size[0]/2) - extents.width/2,
+			mm_to_points(page_size[1] - border[1]/2)
 		)
 		ctx.show_text(title)
 
-def draw_rect_portrait_a4(ctx, markers, title=""):
-	return draw_rect_portrait(ctx, markers, title,
-		page_width_mm=DIN_A4_WIDTH_MM, page_height_mm=DIN_A4_HEIGHT_MM,
-		page_width_points=DIN_A4_WIDTH_POINTS)
+def draw_rect_landscape_a5(ctx, markers, title=""):
+	tl = create_monochrome_surface(markers[0], markers[0].shape, 0)
+	tr = create_monochrome_surface(markers[1], markers[1].shape, 1)
+	br = create_monochrome_surface(markers[2], markers[2].shape, 2)
+	bl = create_monochrome_surface(markers[3], markers[3].shape, 3)
 	
-def draw_rect_portrait_a5(ctx, markers, title=""):
-	return draw_rect_portrait(ctx, markers, title,
-		page_width_mm=DIN_A5_WIDTH_MM, page_height_mm=DIN_A5_HEIGHT_MM,
-		page_width_points=DIN_A5_WIDTH_POINTS)
+	page_size = (DIN_A4_WIDTH_MM, DIN_A4_HEIGHT_MM)
+	border = (10, 10)
+	marker_size = (30, 30)
+
+	tl_pos = (border[0], border[1])
+	tr_pos = (page_size[0] - marker_size[0] - border[0], border[1])
+	br_pos = (page_size[0] - marker_size[0] - border[0], page_size[1]/2 - marker_size[1] - border[1])
+	bl_pos = (border[0], page_size[1]/2 - marker_size[1] - border[1])
+
+	marker_size_points = (mm_to_points(marker_size[0]), mm_to_points(marker_size[1]))
+	
+	draw_image(ctx, tl, (mm_to_points(tl_pos[0]), mm_to_points(tl_pos[1])), marker_size_points)
+	draw_image(ctx, tr, (mm_to_points(tr_pos[0]), mm_to_points(tr_pos[1])), marker_size_points)
+	draw_image(ctx, br, (mm_to_points(br_pos[0]), mm_to_points(br_pos[1])), marker_size_points)
+	draw_image(ctx, bl, (mm_to_points(bl_pos[0]), mm_to_points(bl_pos[1])), marker_size_points)
+	
+	if title != "":
+		ctx.set_font_size(15)
+		extents = ctx.text_extents(title)
+		ctx.move_to(
+			mm_to_points(page_size[0]/2) - extents.width/2,
+			mm_to_points(page_size[1]/2 - border[1]/2)
+		)
+		ctx.show_text(title)
 
 class CanvasTexture:
 	def __init__(self, size):
